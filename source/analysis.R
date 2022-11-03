2c7e44d0cedee7db1fbc21e792e146c992763e40
Jennifer <jngomez@uw.edu>
2022-11-02 19:16:46 -0700

    pushing practice

--- source/analysis.R
+++ source/analysis.R
@@ -6,6 +6,7 @@ library(tidyverse)
 # Load the *national level* data into a variable. `national`
 national <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv")
 
+View(national)
 # This function returns total cases in the US
 get_total_cases <- function() {
   total_cases <- national %>%
@@ -13,7 +14,7 @@ get_total_cases <- function() {
     pull(cases)
   return(prettyNum(total_cases,big.mark=",",scientific=FALSE))
 }
-
+get_total_cases()
 ?prettyNum()
 
 # This function returns the total number of cases in the US
@@ -23,9 +24,10 @@ get_total_deaths <- function() {
     pull(deaths)
   return(total_deaths)
 }
-
+get_total_deaths()
 # Run the following code to create a plot of cumulative cases over time
 # (we'll explain the ggplot2 syntax next week)
 cases_plot <- ggplot(data = national) +
   geom_line(mapping = aes(x = as.Date(date), y = cases)) +
   labs(x = "Date", y = "Cumulative Cases", title = "U.S. COVID Cases")
+
